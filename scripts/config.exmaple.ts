a26ed3a44a70dbe6f600bbbb6b18f47369c7f6ba
Kenn <zhangxjian@me.com>
2023-02-09 17:54:07 +0800

    update

---
+++ scripts/config.exmaple.ts
@@ -0,0 +1,7 @@
+import type { RefreshTokenRequest } from "strava";
+
+export const refreshTokenRequest: RefreshTokenRequest = {
+  client_id: "client_id",
+  client_secret: "client_secret",
+  refresh_token: "refresh_token",
+};
